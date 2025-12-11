fe9c5738d7924d6975017fe228d505307da85dbb
[dyad] <aymansamih383@gmail.com>
2025-12-11 22:38:52 +0100

    [dyad] wrote 1 file(s)

--- src/pages/CityStadiumsPage.tsx
+++ src/pages/CityStadiumsPage.tsx
@@ -1,4 +1,4 @@
-import { useParams, useNavigate } => "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { MapPin, Building, Landmark, Globe, Mountain, Sun, Ship, Eye, Video, Hotel as HotelIcon } from "lucide-react";
